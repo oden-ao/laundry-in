463d95d24dcf685a21a2e08d691784aaffc16f03
muhamzaah <64425119+muhamzaah@users.noreply.github.com>
2022-05-24 19:14:07 +0700

    Add files via upload

---
+++ firebaseconfig.ts
@@ -0,0 +1,46 @@
+import * as firebase from 'firebase/app'
+require('firebase/auth')
+
+
+const config = {
+  apiKey: "AIzaSyDuKm9NkqgWWiVTB7hjzOtVaDIellzQIKA",
+  authDomain: "laundry-e0bc0.firebaseapp.com",
+  projectId: "laundry-e0bc0",
+  storageBucket: "laundry-e0bc0.appspot.com",
+  messagingSenderId: "750183114702",
+  appId: "1:750183114702:web:95ec47b006e5bc5ca22d9f"
+}
+
+// Initialize Firebase
+firebase.initializeApp(config)
+
+export async function loginUser(username: string, password: string) {
+  
+  const email = `${username}@gmail.com`
+  
+  try { 
+    const res = await firebase.auth().signInWithEmailAndPassword(email, password) 
+    
+
+
+    console.log(res)
+    return true
+  } catch(error) {
+    console.log(error)
+    return false
+  }
+}
+
+export async function registerUser(username: string, password: string){
+  const email = `${username}@gmail.com`
+  try {
+    const res = await firebase.auth().createUserWithEmailAndPassword 
+    (username, password)
+    console.log(res)
+    return true
+    
+  } catch(error){
+    console.log(error)
+    return false
+  }
+}
